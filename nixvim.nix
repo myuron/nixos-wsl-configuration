1df004427961a851a7e8bdc47c8e5a4b0c763e06
myuron <myuron.dev@gmail.com>
2026-04-28 08:33:50 +0000

    chore(nvim):add colorscheme catppuccin

--- nixvim.nix
+++ nixvim.nix
@@ -2,6 +2,13 @@
   programs.nixvim = {
     enable = true;
     vimAlias = true;
+    colorschemes = {
+      catppuccin = { enable = true; };
+      # tokyonight = { enable = true; };
+      # ayu = { enable = true; };
+      # bamboo = { enable = true; };
+      # dracula = { enable = true; };
+    };
     imports = [
       ./nvim/keymap.nix
       ./nvim/plugin.nix
